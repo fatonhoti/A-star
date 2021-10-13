0404f881152a2d24613af7e43490fa6eedcb82a7
Faton Hoti <hotifaton@outlook.com>
2021-10-13 16:44:38 +0200

    Relocate code

--- astar.py
+++ astar.py
@@ -1,16 +1,22 @@
 from sys import exit
 
+from models.grid import Grid
+from settings import *
+
 import pygame
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 800
-SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+SCREEN = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption("A-star")
 
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
+
 def run():
+
+    grid = Grid(SCREEN, SCREEN_SIZE, cell_size=10)
 
     # ----- PYGAME LOOP -----
     while True:
@@ -21,17 +27,26 @@ def run():
                 pygame.quit()
                 exit()
 
-            # Handle left-click
+            # Handle mouse clicks
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
 
-            # Handle right-click
+                if event.button == LEFT_CLICK:
+                    pass
+
+                if event.button == RIGHT_CLICK:
+                    pass
 
             # Handle space-bar
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    pass
 
         # Draw
 
         # Update display
         pygame.display.flip()
-        clock.tick(30)  # Limit to 30 frames per second
+        clock.tick(FPS)
 
 
 if __name__ == "__main__":
